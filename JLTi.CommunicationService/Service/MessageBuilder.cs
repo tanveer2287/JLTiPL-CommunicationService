1e13837b7a8411b5ee65708905e977c6b1d083b8
Shaikh <mnt@asia1.group.local>
2017-08-17 11:35:47 +0530

    Add project files.

---
+++ JLTi.CommunicationService/Service/MessageBuilder.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace JLTi.CommunicationService.Service
+{
+    public class MessageBuilder
+    {
+
+    }
+}
